f01e796da16c2b12d83cf8c08a08f3c5350d0ae5
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2018-08-12 15:44:17 +0530

    Create RepeatFinder.java

---
+++ pro/RepeatFinder.java
@@ -0,0 +1,40 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+class RepeatFinder {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for(int i = 0 ; i < size; i++){
+		    arr[i] = sc.nextInt();
+		}
+		List<Integer> values = new ArrayList<Integer>();
+		for(int i = 0; i < size - 1; i++){
+		    boolean check = true;
+		    for(int j = i+1; j < size ; j++){
+		        if(arr[i] == arr[j]){
+		            check = false;
+		            break;
+		        }
+		    }
+		    if(!check){
+		        values.add(arr[i]);
+		    }
+		}
+		if(values.isEmpty()){
+		    System.out.println("unique");
+		}
+		else{
+		    Collections.sort(values);
+		    for(int i : values){
+		        System.out.print(i + " ");
+		    }
+		}
+	}
+}
