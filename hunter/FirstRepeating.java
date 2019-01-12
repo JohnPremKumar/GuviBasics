d1078cf3b2a7003d53bee637f1d1709f9e177297
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-01-12 12:26:02 +0530

    Update FirstRepeating.java

--- hunter/FirstRepeating.java
+++ hunter/FirstRepeating.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 class FirstRepeating {
 	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
 		int[] in = new int[n];
 		for(int i = 0; i < n; i++){
 		    in[i] = sc.nextInt();
