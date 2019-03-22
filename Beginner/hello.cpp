e3521c730529aa4d82baf9ecfbf6e6ba6707fc01
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-03-22 15:16:53 +0530

    Create hello.cpp

---
+++ Beginner/hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+string repeat(string s, int n) 
+{ 
+     
+    string s1 = s; 
+  
+    for (int i=1; i<n;i++) 
+        s += s1; 
+  
+    return s; 
+} 
+  
+int main() 
+{ 
+    string s="hello\n";
+    
+    int n,m ;
+    cin>>n;
+    cout << repeat(s, n) << endl;; 
+    return 0; 
+} 
