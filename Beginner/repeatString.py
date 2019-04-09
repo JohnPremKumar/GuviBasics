35f88763d7586c9fce002958e4f2ac2db11ddaa6
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-04-09 19:48:49 +0530

    Update repeatString.py

--- Beginner/repeatString.py
+++ Beginner/repeatString.py
@@ -1,4 +1,7 @@
 raw = input()
 raw = raw.split(" ")
 for i in range(0,int(raw[1])):
-    print(raw[0], end = "")
+    if(i == int(raw[1])-1):
+        print(raw[0], end = "")
+    else:
+        print(raw[0], end = "<br>")
