2360fa42d4c1f7e7aefd6ccfef14177f6015be95
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-04-21 18:54:30 +0530

    Update hello.cpp

--- Beginner/hello.cpp
+++ Beginner/hello.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 using namespace std;
 
-string repeat(string s, int n) 
-{ 
-     
-    string s1 = s; 
-  
-    for (int i=1; i<n;i++) 
-        s += s1; 
-  
-    return s; 
-} 
-  
-int main() 
-{ 
-    string s="Hello\n";
-    
-    int n,m ;
-    cin>>n;
-    cout << repeat(s, n) << endl;
-    return 0; 
-} 
+int main() {
+    int in;
+	cin>>in;
+	for(int i = 0; i < in; i++){
+	    cout<<"Hello"<<endl;
+	}
+}
