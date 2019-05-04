f9301a3093ab3573d9009efcc39b49c15df9b71e
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-05-04 13:07:28 +0530

    Update test.c

--- Beginner/test.c
+++ Beginner/test.c
@@ -1,11 +1,55 @@
 int main() {
-    int hh, mm, ss ;
-    char t12[3];
-    scanf("%d:%d:%d%s", &hh, &mm, &ss, t12) ;
-        
-    if (strcmp(t12,"PM")==0 && hh!=12) hh += 12 ;
-    if (strcmp(t12,"AM")==0 && hh==12) hh = 0 ;
-        
-    printf("%02d:%02d:%02d", hh, mm, ss) ;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int rows,cols,rot;
+    scanf("%d%d%d",&rows,&cols,&rot);
+    int arr[rows][cols];
+    int result[rows][cols];
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            scanf("%d", &arr[r][c]);
+        }
+    }
+
+    rows--;
+    cols--;
+    for (int r = 0; r <= rows; r++) {
+        for (int c = 0; c <= cols; c++) {
+            int x = r < rows - r ? r : rows - r;
+            int y = c < cols - c ? c : cols - c;
+            int min = x < y ? x : y;
+            int maxRow = rows - min;
+            int maxCol = cols - min;
+            int parameter = (maxRow + maxCol) * 2 - min * 4;
+
+            int row = r;
+            int col = c;
+            for (int a = 0; a < rot%parameter; a++) {
+                if (col == min && row < maxRow) {
+                    row++;
+                }
+                else if (row == maxRow && col < maxCol) {
+                    col++;
+                }
+                else if (row > min && col == maxCol) {
+                    row--;
+                }
+                else if (row == min && col > min) {
+                    col--;
+                }
+            }
+            result[row][col] = arr[r][c];
+        }
+    }
+
+
+    for (int r = 0; r <= rows; r++) {
+        for (int c = 0; c <= cols; c++) {
+            printf("%d ", result[r][c]);
+        }
+        printf("\n");
+    }
+
+
     return 0;
 }
