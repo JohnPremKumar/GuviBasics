3f666a624e5b247a5f6c6b9ce31f912b229e9e34
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-05-04 15:09:27 +0530

    Update test.py

--- Beginner/test.py
+++ Beginner/test.py
@@ -1,41 +1,57 @@
-from math import inf
-from bisect import bisect_left as bLeft, bisect_right as bRight
-from collections import defaultdict
+#include<iostream>
+#include<cstdio>
+using namespace std;
+long long ans=0;
+void mergei(int arr[],int l,int r,int mid)
+{
+    int ni=mid-l+1;
+    int nj=r-mid;
+    int L[ni],R[nj];
+    for(int i=0;i<ni;i++)
+    L[i]=arr[i+l];
+    for(int j=0;j<nj;j++)
+    R[j]=arr[j+mid+1];
+    int i=0,j=0,k=l;
+    while(i<ni && j<nj)
+    {
+        if(L[i]<=R[j])
+        arr[k++]=L[i++];
+        else
+        {
+            arr[k++]=R[j++];
+            ans+=(ni-i);
+        }
+    }
+    for(;i<ni;)
+        arr[k++]=L[i++];
+    for(;j<nj;)
+        arr[k++]=R[j++];
+}
 
-# ------------------------------------------------------------------------------
-def getHealth(seq, first, last, largest):
-  h, ls = 0, len(seq)
-  for f in range(ls):
-    for j in range(1, largest+1):
-      if f+j > ls: break
-      sub = seq[f:f+j]
-      if sub not in subs: break
-      if sub not in gMap: continue
-      ids, hs = gMap[sub]
-      h += hs[bRight(ids, last)]-hs[bLeft(ids, first)]
-  return h
-
-# ------------------------------------------------------------------------------
-howGenes = int(input())
-genes = input().rstrip().split()
-healths = list(map(int, input().rstrip().split()))
-howStrands = int(input())
-gMap = defaultdict(lambda: [[], [0]])
-subs = set()
-for id, gene in enumerate(genes):
-  gMap[gene][0].append(id)
-  for j in range(1, min(len(gene), 500)+1): subs.add(gene[:j])
-for v in gMap.values():
-  for i, ix in enumerate(v[0]): v[1].append(v[1][i]+healths[ix])
-
-# ------------------------------------------------------------------------------
-largest = max(list(map(len, genes)))
-hMin, hMax = inf, 0
-for _ in range(howStrands):
-  firstLastd = input().split()
-  first = int(firstLastd[0])
-  last = int(firstLastd[1])
-  strand = firstLastd[2]
-  h = getHealth(strand, first, last, largest)
-  hMin, hMax = min(hMin, h), max(hMax, h)
-print(hMin, hMax)
+void m_sort(int a[],int i,int j)
+{
+    int mid=(i+j)/2;
+    if(i<j)
+    {
+        m_sort(a,i,mid);
+        m_sort(a,mid+1,j);
+        mergei(a,i,j,mid);
+    }
+}
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        ans=0;
+        scanf("%d",&n);
+        int * a = new int[n];
+        for(int i=0;i<n;i++)
+            scanf("%d",&a[i]);
+        m_sort(a,0,n-1);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
