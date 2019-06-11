45be939cc0d5098540e9e07e78b30864400cb3a4
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-06-11 11:02:53 +0530

    Update Idee.java

--- Idee.java
+++ Idee.java
@@ -1,24 +1,12 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-public class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		if(input > 0){
-			System.out.println("Positive");
-		}
-		else if(input < 0){
-			System.out.println("Negative");
-		}
-		else{
-			System.out.println("Zero");
-		}
-	}
+#include<stdio.h>
+int main(){
+	int num;
+	scanf("%d", &num);
+	if(num == 0)
+	printf("Zero");
+	else if(num > 0)
+	printf("Positive");
+	else
+	printf("Negative");
+	return 1;
 }
