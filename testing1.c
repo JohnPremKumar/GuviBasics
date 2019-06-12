5238da966f6d342b938bd2b3ba2934312c401f56
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-06-12 17:53:28 +0530

    Create testing1.c

---
+++ testing1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int i;
+    char ch[100];
+    for(i=0;i<=7;i++)
+    {
+    scanf("%c",&ch[i]);
+    }
+    for(i=7;i>=0;i--)
+    {
+        printf("%c",ch[i]);
+    }
+	return 0;
+}
+//
