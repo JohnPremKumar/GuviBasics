798f2f45539a8c004d7a24aec55c095d447c7eea
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-07-07 17:43:37 +0530

    Update Smallest123.java

--- Smallest123.java
+++ Smallest123.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 //
-class Smallest {
+public class Smallest {
 	public static void main (String[] args) {
 		Scanner s = new Scanner(System.in);
 		String input = s.next();
