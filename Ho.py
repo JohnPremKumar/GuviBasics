5b217d76bd7c661d8b67f5c9651f11239c277d72
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-07-08 23:27:45 +0530

    Update Ho.py

--- Ho.py
+++ Ho.py
@@ -1,4 +1,13 @@
-x = input()
-y = input()
-print(x.find(y))
-#
+import statistics as st
+ana=int(input())
+l=list(map(int,input().split()))
+cac=0
+for i in range(1,ana):
+    l1=l[0:i]
+    l2=l[i:ana]
+    if(st.mean(l1)==st.mean(l2)):
+        cac=cac+1
+if(cac>=1):
+    print("yes")
+else:
+    print("no")
