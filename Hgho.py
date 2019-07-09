2e69b63a107afb7c8d8c3be95911441f445465ed
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-07-09 16:07:05 +0530

    Update Hgho.py

--- Hgho.py
+++ Hgho.py
@@ -1,13 +1,4 @@
-p=input()
-b=[int(m) for m in input().split()]
-d=[]
-flag=0
-for i in set(b):
-    if(b.count(i)!=1):
-        d.append(i)
-        flag=1
-if(flag==1):
-    print(*sorted(d))
-else:
-    print("unique")
-#
+num1=input().split()
+for i in num1:
+    print(i[::-1],end=' ')
+##
