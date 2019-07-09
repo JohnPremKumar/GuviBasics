0d0610a1a83deba9778b89249f2cb6d08885b33b
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-07-09 09:34:56 +0530

    Update and rename Hho.py to Hgho.py

---
+++ Hgho.py
@@ -0,0 +1,13 @@
+p=input()
+b=[int(m) for m in input().split()]
+d=[]
+flag=0
+for i in set(b):
+    if(b.count(i)!=1):
+        d.append(i)
+        flag=1
+if(flag==1):
+    print(*sorted(d))
+else:
+    print("unique")
+#
