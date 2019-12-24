49774a07505e22990f24563fe6bae4a1a940417d
John Prem Kumar S <36818969+JohnPremKumar@users.noreply.github.com>
2019-12-24 15:50:04 +0530

    Update testing1.c

--- testing1.c
+++ testing1.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	int i;
-    char ch[100];
-    for(i=0;i<=7;i++)
-    {
-    scanf("%c",&ch[i]);
-    }
-    for(i=7;i>=0;i--)
-    {
-        printf("%c",ch[i]);
-    }
+	printf("605461912");
 	return 0;
 }
 //
